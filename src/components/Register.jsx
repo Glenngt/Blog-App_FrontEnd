fd55e2923eebdc7826436038727775528fd8b714
Glenngt <glenn.mattackal@gmail.com>
2023-11-18 01:09:25 +0530

    Registration Completed

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,80 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Register = () => {
+    
+  const[registerData,changeRegisterData]=useState(
+    {
+      "name" : "",
+      "pic" : "",
+      "email" : "",
+      "pswd" : ""
+  }
+)
+
+const RegisterHandler=(event)=>{
+    changeRegisterData({...registerData,[event.target.name]:event.target.value})
+}
+
+const regValue=()=>{
+    console.log(registerData)
+    axios.post("http://127.0.0.1:8000/api1/add/",registerData).then(
+        (response)=>{
+            alert(response.data.status)
+        }
+    )
+}
+
   return (
-    <div>Register</div>
+    <div>
+        
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                        <label htmlFor="" className="form-label">Name :</label>
+                        <input type="text" className="form-control" name="name" value={registerData.name} onChange={RegisterHandler} />
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                        
+                        <label htmlFor="" className="form-label">Profile Pic :</label>
+                        <input type="file" name="pic" id="" className="form-control" value={registerData.pic} onChange={RegisterHandler} />
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                        
+                        <label htmlFor="" className="form-label">Email Id :</label>
+                        <input type="text" className="form-control" name="email" value={registerData.email} onChange={RegisterHandler} />
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                        
+                        <label htmlFor="" className="form-label">Password :</label>
+                        <input type="password" name="pswd" id="" className="form-control" value={registerData.pswd} onChange={RegisterHandler} />
+
+                    </div>
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button onClick={regValue} className="btn btn-primary">REGISTER</button>
+                        </div>
+                    </div>
+                    
+                    <div className="col col-12 col-sm-12col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <a href="/">Back To Login</a>
+
+                    </div>
+                </div>
+
+
+                </div>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
